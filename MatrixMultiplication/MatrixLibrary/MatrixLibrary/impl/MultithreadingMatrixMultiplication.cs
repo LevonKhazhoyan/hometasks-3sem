dde141ebd314e058fa87ba54bf5e3bb42c685c10
LevonKhazhoyan <levon_hazhoyan@mail.ru>
2022-09-21 22:51:26 +0300

    MatrixLibrary is ready

---
+++ MatrixMultiplication/MatrixLibrary/MatrixLibrary/impl/MultithreadingMatrixMultiplication.cs
@@ -0,0 +1,56 @@
+﻿namespace MatrixLibrary.impl;
+
+public class ParallelMatrixMatrixMultiplication : IMatrixMultiplication
+{   
+    /// <summary>
+    /// Multi-threaded realisation of matrix multiplication.
+    /// </summary>
+    /// <param name="first"> First matrix to multiply.</param>
+    /// <param name="second"> Second matrix to multiply.</param>
+    /// <returns> Matrix multiplication product.</returns>
+    /// <exception cref="ArgumentException"> Expected that columns of first matrix is equal to rows of second matrix.</exception>
+    public Matrix Multiply(Matrix first, Matrix second)
+    {
+        var (rows1, cols1) = (first.RowNumber, first.ColNumber);
+        var (rows2, cols2) = (second.RowNumber, second.ColNumber);
+        if (cols1 != rows2)
+        {
+            throw new ArgumentException($"Invalid matrices to multiply - first cols: {first.ColNumber}, " 
+                + $"second rows: {second.RowNumber}"); 
+        }
+
+        var threads = new Thread[Math.Min(Environment.ProcessorCount, rows1)];
+        var chunkSize = rows1 / threads.Length + 1;
+        var result = new Matrix(rows1, cols2);
+
+        for (var i = 0; i < threads.Length; i++)
+        {
+            var localI = i;
+            threads[i] = new Thread(() =>
+            {
+                for (var row = localI * chunkSize; row < Math.Min((localI + 1) * chunkSize, rows1); row++)
+                {
+                    for (var col = 0; col < cols2; col++)
+                    {
+                        for (var j = 0; j < cols1; j++)
+                        {
+                            result[row, col] += first[row, j] * second[j, col];
+                        }
+                    }
+                }
+            });
+        }
+
+        foreach (var thread in threads)
+        {
+            thread.Start();
+        }
+
+        foreach (var thread in threads)
+        {
+            thread.Join();
+        }
+
+        return result;
+    }
+}
---
+++ MatrixMultiplication/MatrixLibrary/MatrixLibrary/impl/SequentialMatrixMultiplication.cs
@@ -0,0 +1,41 @@
+﻿namespace MatrixLibrary.impl;
+
+/// <summary>
+/// Sequential multiplication strategy.
+/// </summary>
+public class SequentialMatrixMultiplication : IMatrixMultiplication
+{
+    /// <summary>
+    /// Single-threaded realisation of matrix multiplication.
+    /// </summary>
+    /// <param name="first"> First matrix to multiply.</param>
+    /// <param name="second"> Second matrix to multiply.</param>
+    /// <returns> Matrix multiplication product.</returns>
+    /// <exception cref="ArgumentException"> Expected that columns of first matrix is equal to rows of second matrix.</exception>
+    public Matrix Multiply(Matrix first, Matrix second)
+    {
+        var (rows1, cols1) = (first.RowNumber, first.ColNumber);
+        var (rows2, cols2) = (second.RowNumber, second.ColNumber);
+
+        if (cols1 != rows2)
+        {
+            throw new ArgumentException($"Invalid matrices sizes - first cols: {first.ColNumber}, " + 
+                $"second lines: {second.RowNumber}");
+        }
+
+        var result = new Matrix(rows1, cols2);
+
+        for (var row = 0; row < rows1; row++)
+        {
+            for (var col = 0; col < cols2; col++)
+            {
+                for (var i = 0; i < cols1; i++)
+                {
+                    result[row, col] += first[row, i] * second[i, col];
+                }
+            }
+        }
+
+        return result;
+    }
+}
